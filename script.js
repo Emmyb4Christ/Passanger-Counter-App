2312b53772c8c21085a57a22e92b38293039c384
Emmyb4Christ <tijanibamidele0@gmail.com>
2022-08-09 22:33:11 +0100

    Passanger Counter App Complete

---
+++ script.js
@@ -0,0 +1,23 @@
+//document.getElementById("count-el").innerText = 10;
+
+//PROCEDURE
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+let countEl = document.getElementById("count-el"); // pass in arguments
+let saveEl = document.getElementById("save-el");
+
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += " " + countStr;
+  countEl.textContent = 0;
+  count - 0;
+}
